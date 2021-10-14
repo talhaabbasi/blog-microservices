5a239363a010d08dc50530912e5220932bd73b52
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-10-14 16:53:06 +0500

    Add post and comments events

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const { randomBytes } = require("crypto")
 const cors = require("cors")
+const axios = require("axios")
 
 const app = express()
 app.use(bodyParser.json())
@@ -14,7 +15,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[id] || [])
 })
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex")
   const { id } = req.params
   const { content } = req.body
@@ -24,6 +25,15 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content })
 
   commentsByPostId[id] = comments
+
+  await axios.post("http://localhost:4005/events", {
+    type: "commentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: id,
+    },
+  })
 
   res.status(201).send(comments)
 })
