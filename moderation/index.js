f546c987dbd11a5fb969deb3b53021ad6a4931b1
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-10-19 12:41:59 +0500

    Handle comments post request

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,26 @@ const bodyParser = require("body-parser")
 const app = express()
 app.use(bodyParser.json())
 
-app.post("/events", (req, res) => {})
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved"
+    await axios
+      .post("http://localhost:4005/events", {
+        type: "CommentModerated",
+        data: {
+          id: data.id,
+          postId: data.postId,
+          status,
+          content: data.content,
+        },
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }
+  res.send({})
+})
 
 app.listen(4003, () => {
   console.log("Listening on 4003")
