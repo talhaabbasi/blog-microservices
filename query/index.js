ae1ba66eb68315b1198a71849e920b592fc25fb0
talhaabbasi <talhaabbasi1997@gmail.com>
2021-11-18 20:28:10 +0500

    Add images for remaining services

--- query/index.js
+++ query/index.js
@@ -51,9 +51,11 @@ app.post("/events", (req, res) => {
 app.listen(4002, async () => {
   console.log("Listening on 4002")
   try {
-    const res = await axios.get("http://localhost:4005/events").catch((err) => {
-      console.log(err.message)
-    })
+    const res = await axios
+      .get("http://event-bus-srv:4005/events")
+      .catch((err) => {
+        console.log(err.message)
+      })
 
     for (let event of res.data) {
       console.log("Processing event:", event.type)
